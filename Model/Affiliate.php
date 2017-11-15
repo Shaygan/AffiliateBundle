428496205ce3132e8b21313a513ee30aa932a7d8
Iman Ghasrfakhri <ghasrfakhri@gmail.com>
2017-11-15 17:24:11 +0330

    Rearrange methods

--- Model/Affiliate.php
+++ Model/Affiliate.php
@@ -21,12 +21,12 @@ use Symfony\Component\HttpFoundation\Response;
 class Affiliate
 {
 
-    protected $em;
-    protected $session;
-    protected $request;
-    protected $cookies;
-    protected $dispatcher;
-    protected $config;
+    private $em;
+    private $session;
+    private $request;
+    private $cookies;
+    private $dispatcher;
+    private $config;
 
     function __construct(EntityManager $em, Container $container)
     {
@@ -52,7 +52,7 @@ class Affiliate
      */
     function record($response)
     {
-        $referrerId = (int) $this->getRefParam();
+        $referrerId = (int)$this->getRefParam();
         if ($referrerId) {
             if (!$this->session->has($this->config['session_referral_id_param_name'])) {
                 $this->logReferral($referrerId, $response);
@@ -67,184 +67,49 @@ class Affiliate
         }
     }
 
-    public function createReferrer($referrerId)
+    private function getRefParam()
     {
-        $referrer = new Referrer();
-        $referrer->setId($referrerId);
-        $referrer->setReferCount(1);
-
-        $this->em->persist($referrer);
-        $this->em->flush();
-    }
-
-    /**
-     * Check referral logs if found referral record the user as referred for
-     * next events
-     *
-     * @param Response $response
-     * @param User $user
-     */
-    public function recordRegistration(Response $response, User $user)
-    {
-        if ($this->hasReferral()) {
-            $referral = $this->getReferral();
-            if ($referral !== null && $referral->getRegistration() == null) {
-                $this->saveRegistrationLog($user, $referral);
-                $this->getDispatcher()->dispatch(
-                        ShayganAffiliateEvents::REGISTER_COMPLETED
-                        , new GetReferralRegistrationEvent($referral, $user)
-                );
-            }
-            $this->clearReferral($response);
-        }
-    }
-
-    /**
-     * @param  PurchaseInterface $order
-     * @param string $program
-     * @return Purchase | null
-     */
-    public function getPurchaseCommission(PurchaseInterface $order, $program = "default")
-    {
-        if ($this->isPurchaseEligible($order->getReferredUser(), $program)) {
-            $commission = $this->createPurchaseEntity($order, $program);
-            $this->em->persist($commission);
-            $this->em->flush();
-            $this->getDispatcher()->dispatch(
-                    ShayganAffiliateEvents::PURCHASE_COMPLETED
-                    , new GetPurchaseEvent($commission)
-            );
-            return $commission;
-        } else {
-            return null;
-        }
-    }
-
-    public function getRegistrationCountByUser(User $user)
-    {
-        return $this->em->getRepository(ReferralRegistration::class)->getRegistrationCountByUser($user);
-    }
-
-    /**
-     *
-     * @param PurchaseInterface $order
-     * @param string $program
-     * @return Purchase
-     */
-    protected function createPurchaseEntity(PurchaseInterface $order, $program)
-    {
-        $type = $this->config['programs'][$program]['type'];
-        $referralRegistration = $this->getUserReferralRegistration($order->getReferredUser());
-        $purchasePrice = $order->getPurchasePrice();
-
-
-        $purchase = new Purchase();
-        $purchase->setProgram($program);
-        $purchase->setType($type);
-        $purchase->setOrderId($order->getId());
-        $purchase->setReferralRegistration($referralRegistration);
-        $purchase->setReferrer($referralRegistration->getReferrer());
-        $purchase->setPurchaseAmount($purchasePrice);
-        $purchase->setCommissionAmount($this->getCommissionAmount($order, $program));
-        $purchase->setCommission($this->getCommissionValue($order, $program));
-
-        return $purchase;
-    }
-
-    private function getCommissionAmount(PurchaseInterface $order, $program)
-    {
-        $type = $this->config['programs'][$program]['type'];
-        $totalPrice = $order->getPurchasePrice();
-        if ($type == "percentage") {
-            if ($this->isFirstPurchase($order->getReferredUser(), $program)) {
-                $commissionAmount = (int) ($totalPrice * ($this->config['programs'][$program]['first_commission_percent'] / 100));
-            } else {
-                $commissionAmount = (int) ($totalPrice * ($this->config['programs'][$program]['commission_percent'] / 100));
-            }
-        } elseif ($type == "fixed-price") {
-            if ($this->isFirstPurchase($order->getReferredUser(), $program)) {
-                $commissionAmount = $this->config['programs'][$program]['first_commission_amount'];
-            } else {
-                $commissionAmount = $this->config['programs'][$program]['first_commission_amount'];
-            }
-        } else {
-            throw new \Exception("Invalid commission type.");
+        $key = $this->config['referrer_param_name'];
+        if (filter_input(INPUT_GET, $key, FILTER_VALIDATE_INT) > 0) {
+            return filter_input(INPUT_GET, $key, FILTER_VALIDATE_INT);
         }
 
-        return $commissionAmount;
-    }
-
-    private function getCommissionValue(PurchaseInterface $order, $program)
-    {
-        $type = $this->config['programs'][$program]['type'];
-        if ($type == "percentage") {
-            if ($this->isFirstPurchase($order->getReferredUser(), $program)) {
-                $commissionValue = $this->config['programs'][$program]['first_commission_percent'];
-            } else {
-                $commissionValue = $this->config['programs'][$program]['commission_percent'];
+        $altKeys = $this->config['referrer_alternative_param_names'];
+        foreach ($altKeys as $key) {
+            if (filter_input(INPUT_GET, $key, FILTER_VALIDATE_INT) > 0) {
+                return filter_input(INPUT_GET, $key, FILTER_VALIDATE_INT);
             }
-        } elseif ($type == "fixed-price") {
-            if ($this->isFirstPurchase($order->getReferredUser(), $program)) {
-                $commissionValue = $this->config['programs'][$program]['first_commission_amount'];
-            } else {
-                $commissionValue = $this->config['programs'][$program]['first_commission_amount'];
-            }
-        } else {
-            throw new \Exception("Invalid commission type.");
         }
 
-        return $commissionValue;
+        return 0;
     }
 
-    public function getReferrerIfPurchaseCommissionEligible(User $user, $program)
+    private function logReferral($referrerId, Response $response)
     {
-        if ($this->isPurchaseEligible($user, $program)) {
-            return $this->getUserReferralRegistration($user);
+        $q = $this->em->createQuery('UPDATE ShayganAffiliateBundle:Referrer r SET r.referCount=r.referCount+1 WHERE r.id = :referrerId');
+        $q->setParameters(array(
+            'referrerId' => $referrerId,
+        ));
+        $rowCount = $q->execute();
+        if ($rowCount == 1) {
+            $referrer = $this->getReferrer($referrerId);
+            $referral = $this->createReferral($referrer);
+            $this->setSession($referral->getId());
+            $this->setCookie($response, $referral->getId());
         } else {
-            return false;
+            $this->createReferrer($referrerId);
+            $this->logReferral($referrerId, $response);
         }
     }
 
     /**
-     * @param User $user
-     * @param string $program
-     * @return bool
+     * @param $referrerId
+     * @return Referrer
      */
-    protected function isPurchaseEligible(User $user, $program)
+    private function getReferrer($referrerId)
     {
-        $reg = $this->getUserReferralRegistration($user);
-
-        if ($reg === null) {
-            return false;
-        }
-
-        if ($reg->getPurchaseCountByProgram($program) < $this->config['programs'][$program]['max_count']) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    protected function isFirstPurchase(User $user, $program)
-    {
-        $reg = $this->getUserReferralRegistration($user);
-
-        if ($reg === null) {
-            return false;
-        }
-
-        return $reg->getPurchaseCountByProgram($program) == 0;
-    }
-
-    /**
-     *
-     * @param User $user
-     * @return ReferralRegistration
-     */
-    protected function getUserReferralRegistration(User $user)
-    {
-        return $this->em->getRepository(ReferralRegistration::class)
-                        ->findOneBy(array("userId" => $user->getId()));
+        $referrer = $this->em->getRepository(Referrer::class)->find($referrerId);
+        return $referrer;
     }
 
     /**
@@ -260,16 +125,6 @@ class Affiliate
         $this->em->persist($referral);
         $this->em->flush();
         return $referral;
-    }
-
-    /**
-     * @param $referrerId
-     * @return Referrer
-     */
-    private function getReferrer($referrerId)
-    {
-        $referrer = $this->em->getRepository(Referrer::class)->find($referrerId);
-        return $referrer;
     }
 
     private function getReferrerUrl()
@@ -304,44 +159,67 @@ class Affiliate
         return null;
     }
 
-    protected function logReferral($referrerId, Response $response)
-    {
-        $q = $this->em->createQuery('UPDATE ShayganAffiliateBundle:Referrer r SET r.referCount=r.referCount+1 WHERE r.id = :referrerId');
-        $q->setParameters(array(
-            'referrerId' => $referrerId,
-        ));
-        $rowCount = $q->execute();
-        if ($rowCount == 1) {
-            $referrer = $this->getReferrer($referrerId);
-            $referral = $this->createReferral($referrer);
-            $this->setSession($referral->getId());
-            $this->setCookie($response, $referral->getId());
-        } else {
-            $this->createReferrer($referrerId);
-            $this->logReferral($referrerId, $response);
-        }
-    }
-
-    protected function setSession($referralId)
+    private function setSession($referralId)
     {
         $this->session->set($this->config['session_referral_id_param_name'], $referralId);
     }
 
-    protected function setCookie($response, $referralId)
+    private function setCookie($response, $referralId)
     {
         $response->headers
-                ->setCookie(new Cookie($this->config['cookie_referral_id_param_name']
-                        , $referralId
-                        , time() + $this->config['cookie_expire_in']
-                        , $this->config['cookie_path']
-                        , null
-                        , $this->config['cookie_secure']
-                        , $this->config['cookie_httponly']
-                        )
-        );
+            ->setCookie(new Cookie($this->config['cookie_referral_id_param_name']
+                    , $referralId
+                    , time() + $this->config['cookie_expire_in']
+                    , $this->config['cookie_path']
+                    , null
+                    , $this->config['cookie_secure']
+                    , $this->config['cookie_httponly']
+                )
+            );
     }
 
-    protected function hasReferral()
+    public function createReferrer($referrerId)
+    {
+        $referrer = new Referrer();
+        $referrer->setId($referrerId);
+        $referrer->setReferCount(1);
+
+        $this->em->persist($referrer);
+        $this->em->flush();
+    }
+
+    /**
+     *
+     * @return Request
+     */
+    private function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Check referral logs if found referral record the user as referred for
+     * next events
+     *
+     * @param Response $response
+     * @param User $user
+     */
+    public function recordRegistration(Response $response, User $user)
+    {
+        if ($this->hasReferral()) {
+            $referral = $this->getReferral();
+            if ($referral !== null && $referral->getRegistration() == null) {
+                $this->saveRegistrationLog($user, $referral);
+                $this->getDispatcher()->dispatch(
+                    ShayganAffiliateEvents::REGISTER_COMPLETED
+                    , new GetReferralRegistrationEvent($referral, $user)
+                );
+            }
+            $this->clearReferral($response);
+        }
+    }
+
+    private function hasReferral()
     {
         return $this->session->has($this->config['session_referral_id_param_name']);
     }
@@ -350,14 +228,14 @@ class Affiliate
      *
      * @return Referral
      */
-    protected function getReferral()
+    private function getReferral()
     {
         $referralId = $this->session->get($this->config['session_referral_id_param_name']);
         $referral = $this->em->getRepository(Referral::class)->find($referralId);
         return $referral;
     }
 
-    protected function saveRegistrationLog(User $user, Referral $referral)
+    private function saveRegistrationLog(User $user, Referral $referral)
     {
         $reg = new ReferralRegistration();
         $reg->setUserId($user->getId());
@@ -370,27 +248,167 @@ class Affiliate
         return $reg;
     }
 
-    protected function clearReferral(Response $response)
+    /**
+     *
+     * @return EventDispatcher
+     */
+    private function getDispatcher()
+    {
+        return $this->dispatcher;
+    }
+
+    private function clearReferral(Response $response)
     {
         $this->session->remove($this->config['session_referral_id_param_name']);
         $response->headers->clearCookie($this->config['cookie_referral_id_param_name']);
     }
 
-    protected function getRefParam()
+    /**
+     * @param  PurchaseInterface $order
+     * @param string $program
+     * @return Purchase | null
+     */
+    public function getPurchaseCommission(PurchaseInterface $order, $program = "default")
     {
-        $key = $this->config['referrer_param_name'];
-        if (filter_input(INPUT_GET, $key, FILTER_VALIDATE_INT) > 0) {
-            return filter_input(INPUT_GET, $key, FILTER_VALIDATE_INT);
+        if ($this->isPurchaseEligible($order->getReferredUser(), $program)) {
+            $commission = $this->createPurchaseEntity($order, $program);
+            $this->em->persist($commission);
+            $this->em->flush();
+            $this->getDispatcher()->dispatch(
+                ShayganAffiliateEvents::PURCHASE_COMPLETED
+                , new GetPurchaseEvent($commission)
+            );
+            return $commission;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param User $user
+     * @param string $program
+     * @return bool
+     */
+    private function isPurchaseEligible(User $user, $program)
+    {
+        $reg = $this->getUserReferralRegistration($user);
+
+        if ($reg === null) {
+            return false;
         }
 
-        $altKeys = $this->config['referrer_alternative_param_names'];
-        foreach ($altKeys as $key) {
-            if (filter_input(INPUT_GET, $key, FILTER_VALIDATE_INT) > 0) {
-                return filter_input(INPUT_GET, $key, FILTER_VALIDATE_INT);
+        if ($reg->getPurchaseCountByProgram($program) < $this->config['programs'][$program]['max_count']) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param User $user
+     * @return ReferralRegistration
+     */
+    private function getUserReferralRegistration(User $user)
+    {
+        return $this->em->getRepository(ReferralRegistration::class)
+            ->findOneBy(array("userId" => $user->getId()));
+    }
+
+    /**
+     *
+     * @param PurchaseInterface $order
+     * @param string $program
+     * @return Purchase
+     */
+    private function createPurchaseEntity(PurchaseInterface $order, $program)
+    {
+        $type = $this->config['programs'][$program]['type'];
+        $referralRegistration = $this->getUserReferralRegistration($order->getReferredUser());
+        $purchasePrice = $order->getPurchasePrice();
+
+
+        $purchase = new Purchase();
+        $purchase->setProgram($program);
+        $purchase->setType($type);
+        $purchase->setOrderId($order->getId());
+        $purchase->setReferralRegistration($referralRegistration);
+        $purchase->setReferrer($referralRegistration->getReferrer());
+        $purchase->setPurchaseAmount($purchasePrice);
+        $purchase->setCommissionAmount($this->getCommissionAmount($order, $program));
+        $purchase->setCommission($this->getCommissionValue($order, $program));
+
+        return $purchase;
+    }
+
+    private function getCommissionAmount(PurchaseInterface $order, $program)
+    {
+        $type = $this->config['programs'][$program]['type'];
+        $totalPrice = $order->getPurchasePrice();
+        if ($type == "percentage") {
+            if ($this->isFirstPurchase($order->getReferredUser(), $program)) {
+                $commissionAmount = (int)($totalPrice * ($this->config['programs'][$program]['first_commission_percent'] / 100));
+            } else {
+                $commissionAmount = (int)($totalPrice * ($this->config['programs'][$program]['commission_percent'] / 100));
             }
+        } elseif ($type == "fixed-price") {
+            if ($this->isFirstPurchase($order->getReferredUser(), $program)) {
+                $commissionAmount = $this->config['programs'][$program]['first_commission_amount'];
+            } else {
+                $commissionAmount = $this->config['programs'][$program]['first_commission_amount'];
+            }
+        } else {
+            throw new \Exception("Invalid commission type.");
         }
 
-        return 0;
+        return $commissionAmount;
+    }
+
+    private function isFirstPurchase(User $user, $program)
+    {
+        $reg = $this->getUserReferralRegistration($user);
+
+        if ($reg === null) {
+            return false;
+        }
+
+        return $reg->getPurchaseCountByProgram($program) == 0;
+    }
+
+    private function getCommissionValue(PurchaseInterface $order, $program)
+    {
+        $type = $this->config['programs'][$program]['type'];
+        if ($type == "percentage") {
+            if ($this->isFirstPurchase($order->getReferredUser(), $program)) {
+                $commissionValue = $this->config['programs'][$program]['first_commission_percent'];
+            } else {
+                $commissionValue = $this->config['programs'][$program]['commission_percent'];
+            }
+        } elseif ($type == "fixed-price") {
+            if ($this->isFirstPurchase($order->getReferredUser(), $program)) {
+                $commissionValue = $this->config['programs'][$program]['first_commission_amount'];
+            } else {
+                $commissionValue = $this->config['programs'][$program]['first_commission_amount'];
+            }
+        } else {
+            throw new \Exception("Invalid commission type.");
+        }
+
+        return $commissionValue;
+    }
+
+    public function getRegistrationCountByUser(User $user)
+    {
+        return $this->em->getRepository(ReferralRegistration::class)->getRegistrationCountByUser($user);
+    }
+
+    public function getReferrerIfPurchaseCommissionEligible(User $user, $program)
+    {
+        if ($this->isPurchaseEligible($user, $program)) {
+            return $this->getUserReferralRegistration($user);
+        } else {
+            return false;
+        }
     }
 
     public function saveRegistration(User $user, User $referrerUser)
@@ -404,24 +422,6 @@ class Affiliate
         $this->em->persist($reg);
         $this->em->flush();
         return $reg;
-    }
-
-    /**
-     *
-     * @return Request
-     */
-    protected function getRequest()
-    {
-        return $this->request;
-    }
-
-    /**
-     *
-     * @return EventDispatcher
-     */
-    protected function getDispatcher()
-    {
-        return $this->dispatcher;
     }
 
 }
