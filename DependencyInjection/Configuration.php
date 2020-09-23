1161f3c15b6a3d567c14816dcd593f7217122aef
Iman Ghasrfakhri <ghasrfakhri@gmail.com>
2020-09-23 23:54:11 +0330

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('shaygan_affiliate');
+        $treeBuilder = new TreeBuilder('shaygan_affiliate');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
                 ->children()
