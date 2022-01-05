855eec6f944d754fd2ee2d46ee63f5cbd6c49500
Iman Ghasrfakhri <ghasrfakhri@gmail.com>
2022-01-05 22:34:27 +0330

    Some tweaks

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -2,53 +2,52 @@
 
 namespace Shaygan\AffiliateBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/affiliate")
  */
-class UserController extends AbstractController {
-
+class UserController extends AbstractController
+{
     /**
      * @Route("/", name="shaygan_affiliate_user_index")
-     * @Template()
      */
-    public function indexAction(Request $request) {
+    public function indexAction(Request $request)
+    {
         if ($this->getUser()) {
             $query = $this->getEm()->getRepository('ShayganAffiliateBundle:ReferralRegistration')->getRegistrationByUser($this->getUser());
             $paginator = $this->get('knp_paginator');
             $pagination = $paginator->paginate(
-                    $query, $request->query->getInt('page', 1)/* page number */, 10/* limit per page */
+                $query, $request->query->getInt('page', 1)/* page number */, 10/* limit per page */
             );
 
-
-            return array('pagination' => $pagination);
+            return $this->render('@ShayganAffiliate/user/index.html.twig', ['pagination' => $pagination]);
         } else {
-            return array();
+            return $this->render('@ShayganAffiliate/user/index.html.twig');
         }
     }
 
     /**
      * @Route("/report", name="shaygan_affiliate_user_report")
-     * @Template()
-     * @Security("has_role('ROLE_USER')");
+     * @IsGranted("ROLE_USER")
      */
-    public function reportAction(Request $request) {
+    public function reportAction(Request $request)
+    {
         $query = $this->getEm()->getRepository('ShayganAffiliateBundle:ReferralRegistration')->getRegistrationByUser($this->getUser());
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
-                $query, $request->query->getInt('page', 1)/* page number */, 10/* limit per page */
+            $query, $request->query->getInt('page', 1)/* page number */, 10/* limit per page */
         );
 
-
-        return array('pagination' => $pagination);
+        return $this->render('@ShayganAffiliate/user/report.html.twig', ['pagination' => $pagination]);
     }
 
-    private function getEm() {
+    private function getEm()
+    {
         return $this->getDoctrine()->getManager();
     }
 
